73bb93a080a027e48c21aa18d1be4e60bb59517f
marlon de leon <mdeleong17@miumg.edu.gt>
2026-01-28 10:59:04 -0600

    primer programa

---
+++ miPrimerPrograma/src/main/java/miprimerprograma/MiPrimerPrograma.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miprimerprograma;
+
+/**
+ *
+ * @author daguilae
+ */
+public class MiPrimerPrograma {
+        public static void main(String[] args) {
+            System.out.println("Hola mundo");
+            System.out.println("Marlon Rubén De León González 9959-19-460");
+    }
+}
